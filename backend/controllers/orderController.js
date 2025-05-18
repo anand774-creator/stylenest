aef32f1eceb7ebd08fa32b51e6f55d2c2489f859
anand774-creator <stylenest696@gmail.com>
2025-05-18 20:31:40 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,6 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from 'stripe'
-import razorpay from 'razorpay'
 
 // global variables
 const currency = 'inr'
@@ -10,10 +9,7 @@ const deliveryCharge = 10
 // gateway initialize
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-const razorpayInstance = new razorpay({
-    key_id : process.env.RAZORPAY_KEY_ID,
-    key_secret : process.env.RAZORPAY_KEY_SECRET,
-})
+
 
 // Placing orders using COD Method
 const placeOrder = async (req,res) => {
